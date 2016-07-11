39249359cdf010df327028c9d6ce6c98da4b0efa
Paras <paras.patidar@wittybrains.com>
2016-07-11 19:02:45 +0530

    changes

--- src/main/java/runner/RunMe.java
+++ src/main/java/runner/RunMe.java
@@ -7,8 +7,9 @@ import bindinds.custom.implementation.CustomDriver;
  * @author PARAS<parasp443@gmail.com>
  *
  */
-public class RunnerUtil {
+public class RunMe {
 	public static void main(String... arg) throws Exception {
+		System.out.println("Setup starts");
 		CustomDriver driver = new CustomDriver();
 		driver.get("http://google.co.in");
 		System.out.println(driver.getTitle());
